dda29a6aa53bd3057af17629960a842566b3b452
Josh Walawender <jmwalawender@gmail.com>
2013-07-10 15:53:32 -0400

    Added code to check file size and open vis fits.open

---
+++ segue.py
@@ -0,0 +1,24 @@
+#!python
+
+import sys
+import os
+from astropy.io import fits
+
+
+def get_columns(filename, *args):
+	## Check File Size
+	try:
+		filesize_bytes = os.stat(filename).st_size
+	except OSError:
+		raise OSError("File probably doesn't exist.  Did you put in the correct path?")
+
+	## Open the fits file
+	try:
+		if filesize_bytes < 2**30:
+			hdu = fits.open(filename, memmap=False)
+		else:
+			hdu = fits.open(filename, memmap=True)
+	except IOError:
+		raise IOError("Probably not a fits file.  Try again with the correct file.")
+	
+	
