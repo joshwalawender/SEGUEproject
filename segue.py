a511003d3df83c1121f5bf715f07d362eedf5fad
Josh Walawender <jmwalawender@gmail.com>
2013-07-10 15:59:45 -0400

    Added command line options and main program to call get_columns.

--- segue.py
+++ segue.py
@@ -1,9 +1,26 @@
-#!python
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+untitled.py
+
+Created by Josh Walawender on 2013-07-10.
+Copyright (c) 2013 . All rights reserved.
+"""
 
 import sys
 import os
+import getopt
 from astropy.io import fits
 
+
+help_message = '''
+The help message goes here.
+'''
+
+
+class Usage(Exception):
+	def __init__(self, msg):
+		self.msg = msg
 
 def get_columns(filename, *args):
 	## Check File Size
@@ -20,5 +37,35 @@ def get_columns(filename, *args):
 			hdu = fits.open(filename, memmap=True)
 	except IOError:
 		raise IOError("Probably not a fits file.  Try again with the correct file.")
+
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(argv[1:], "hi:v", ["help", "input="])
+		except getopt.error, msg:
+			raise Usage(msg)
 	
+		# option processing
+		for option, value in opts:
+			if option == "-v":
+				verbose = True
+			if option in ("-h", "--help"):
+				raise Usage(help_message)
+			if option in ("-i", "--input"):
+				filename = value
 	
+	except Usage, err:
+		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+		print >> sys.stderr, "\t for help use --help"
+		return 2
+
+	## Main Program Stats Here
+	data = get_columns(filename)
+
+
+
+if __name__ == "__main__":
+	sys.exit(main())
