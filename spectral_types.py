53e92d1948d3de052cdf1341031f0e31f4c82147
Emma Walker <emma.s.walker@gmail.com>
2013-07-10 16:36:30 -0400

    reorganisation

---
+++ spectral_types.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import getopt
+from astropy.io import fits
+import numpy as np
+import segue
+
+def main(argv=None):
+	filename = None
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(argv[1:], "hi:v", ["help", "input="])
+		except getopt.error, msg:
+			raise Usage(msg)
+	
+		# option processing
+		for option, value in opts:
+			if option == "-v":
+				verbose = True
+			if option in ("-h", "--help"):
+				raise Usage(help_message)
+			if option in ("-i", "--input"):
+				filename = value
+	
+	except Usage, err:
+		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+		print >> sys.stderr, "\t for help use --help"
+		return 2
+
+	## If no -i input set use the first argument as the fits filename
+	if not filename and len(args)>0:
+		filename = args[0]
+	else:
+		sys.exit(1)  ## this should be improved
+	
+	
+	## Main Program Stats Here
+	data = get_columns(filename)
+
+
+
+if __name__ == "__main__":
+	sys.exit(main())
