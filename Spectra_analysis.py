4c8ee0d991a260e5e0c315eec20003fd3ccd044a
VICTOR CALDERON <vcalderon2009@my.fit.edu>
2013-07-12 04:05:39 -0400

    Adding file for Spectral Analysis

---
+++ Spectra_analysis.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import getopt
+import astropy
+from astropy import table
+from astropy.io import fits
+import numpy as np
+import math
+import astropy.units as u
+import astropy.constants as consts
+import urllib2
+import json
+from scipy import interpolate
+import segue
+
+class Spectrum(object):
+	
+	def Spectrum_def(self, spectrum_array):
+		mjd, plate, fiber = spectrum_array
+		#self.plate = plate
+		#self.mjd   = mjd
+		#self.fiber = fiber
+		self.url   = "http://api.sdss3.org/spectrum?plate={0}&mjd={1}&fiber={2}&format=json".format(plate, mjd, fiber)
+		
+		response   = urllib2.urlopen(self.url)
+		spectrum   = json.load(response)
+		return spectrum
+
+## Abbreviation
+S = Spectrum()
+
+## Obtaining data from file
+data_spec = segue.get_columns(filename, 'MJD', 'PLATE', 'FIBER')
+
+## Initializing the array for the spectra
+Spectra_array = []
+
+### Extracts 10 Spectra
+for i in range(0, 10):
+    Spectra_array.append( S.Spectrum_def( data_spec[ i ] ) )
+
+### Turning list into an array
+Spectra_array = np.array( Spectra_array )
